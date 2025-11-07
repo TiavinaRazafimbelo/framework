e295b7f5bd407b4a59b0f7b3cd05d5f96f749569
TiavinaRazafimbelo <razafimbelotiavina7@gmail.com>
2025-11-07 15:10:24 +0100

    Synchronisation fichiers de sprint3

--- src/main/java/com/framework/annotation/MonAnnotation.java
+++ src/main/java/com/framework/annotation/MonAnnotation.java
@@ -5,10 +5,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
-// L’annotation sera visible à l’exécution et utilisable sur les méthodes
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface MonAnnotation {
-    String valeur(); // un seul paramètre obligatoire
+    String url(); // <- c’est cette URL que FrontServlet utilisera
 }
-
