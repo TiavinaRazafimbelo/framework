cc8eea7ea65174c65ff98f09352b6f973230080c
TiavinaRazafimbelo <razafimbelotiavina7@gmail.com>
2025-11-15 10:20:08 +0100

    sprint5

--- src/main/java/com/framework/core/ModelView.java
+++ src/main/java/com/framework/core/ModelView.java
@@ -1,7 +1,10 @@
 package com.framework.core;
 
+import java.util.HashMap;
+
 public class ModelView {
     private String view;
+    private HashMap<String, Object> data = new HashMap<>();
 
     public ModelView(String view) {
         this.view = view;
@@ -9,5 +12,13 @@ public class ModelView {
 
     public String getView() {
         return view;
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void addItem(String key, Object value) {
+        this.data.put(key, value);
     }
 }
